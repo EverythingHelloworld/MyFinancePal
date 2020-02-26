959763d51bc69ed0dedd8135495e034c0e8900c9
EverythingHelloworld <l00088262@student.lyit.ie>
2020-02-26 10:06:29 +0000

    moved todays date function to global functions

--- src/js/globalFunctions.js
+++ src/js/globalFunctions.js
@@ -70,3 +70,15 @@ function incrementLoginAttempts(id) {
 function lockAccount(id) {
     $.post("../php/lockAccount.php", { 'id': id });
 }
+
+//Get today's date
+function todaysDate()
+{
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
+}
--- src/js/registration.js
+++ src/js/registration.js
@@ -171,14 +171,3 @@ window.onload = function()
         window.location.href = 'login.html'
     })
 }
-
-function todaysDate()
-{
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-
-    today = yyyy + '-' + mm + '-' + dd;
-    return today;
-}
