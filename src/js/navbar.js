540becd18b8e3125442fd3e906f118a159c44cb3
EverythingHelloworld <l00088262@student.lyit.ie>
2020-03-06 21:36:14 +0000

    Added payees link to navbar

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -5,7 +5,7 @@ $("document").ready(function () {
                 "</button><div class='collapse navbar-collapse' id='navbarTogglerDemo01'><ul id='list' class='nav nav-pills nav-fill' style='width:100%'>" +
                 "<li class='nav-item'><a class='nav-link' href='../html/main.html' class='external' style='color:rgba(255,255,255,.5);'>Home</a>" +
                 "</li><li class='nav-item'><a class='nav-link' href='../html/analytics.html' class='external' style='color:rgba(255,255,255,.5);'>Analytics</a>" +
-                "</li><li class='nav-item'><a class='nav-link' href='../html/transfer.html' class='external' style='color:rgba(255,255,255,.5);'>Transfer</a>" +
+                "</li><li class='nav-item'><a class='nav-link' href='../html/managePayees.html' class='external' style='color:rgba(255,255,255,.5);'>Payees</a>" +
                 "</li><li class='nav-item'><a class='nav-link' href='../html/loan.html' class='external' style='color:rgba(255,255,255,.5);'>Loan</a>" +
                 "</li></ul></div></nav><br/>");
 
