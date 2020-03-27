cd3c55fa1a02ac53085472698b2992281a6e9ff7
Aaryn MacC <L00088572@student.lyit.ie>
2020-03-27 22:34:43 +0000

    more loan changes

--- src/js/loan.js
+++ src/js/loan.js
@@ -5,6 +5,21 @@ $("document").ready(function () {
     // hide page content and redirect to login page
     redirectToLoginIfCustomerCookieNotSet();
     setActiveNavLink();
+    const session_customer_id = Cookies.get('customerID');
+
+    $.getJSON(`../php/getAccountInfo.php?id=${session_customer_id}`, function (data) 
+    {
+        for (var i = 0; i < data.customer.length; i++) 
+        {
+            $("#accountSelect").append(`<option>${data.customer[i].AccountID}</option>`);
+        }
+    });
+
+    $("#accountSelect").change(function() 
+    {   
+        $("#purpose").css("display", "block");
+        $("#FirstOption").remove();
+    });
 
     $("#loanPurpose").change(function() 
     {   
@@ -17,19 +32,17 @@ $("document").ready(function () {
     $('#amountToBorrow').change(() => {
         selectedAmount = parseFloat($("#amountToBorrow").val());
         $("#1stOption").remove();
-        selectedAmount+0.00;
         $('#repaySelect').empty();
 
         var interest = selectedAmount*0.08
         var totalPlusInterest = selectedAmount+interest;
         console.log(totalPlusInterest);
 
-        $('#repaySelect').append(`<option>\u20AC${totalPlusInterest/12} per week over 1 year</option>`);
-        $('#repaySelect').append(`<option>\u20AC${totalPlusInterest/24} per week over 2 year</option>`);
-        $('#repaySelect').append(`<option>\u20AC${totalPlusInterest/36} per week over 3 year</option>`);
-        $('#repaySelect').append(`<option>\u20AC${totalPlusInterest/48} per week over 4 year</option>`);
-        $('#repaySelect').append(`<option>\u20AC${totalPlusInterest/60} per week over 5 year</option>`);
-
+        $('#repaySelect').append(`<option>\u20AC${totalPlusInterest/12} Per Month Over 1 Year</option>`);
+        $('#repaySelect').append(`<option>\u20AC${totalPlusInterest/24} Per Month Over 2 Year</option>`);
+        $('#repaySelect').append(`<option>\u20AC${totalPlusInterest/36} Per Month Over 3 Year</option>`);
+        $('#repaySelect').append(`<option>\u20AC${totalPlusInterest/48} Per Month Over 4 Year</option>`);
+        $('#repaySelect').append(`<option>\u20AC${totalPlusInterest/60} Per Month Over 5 Year</option>`);
 
         $("#repayAmount").css("display", "block");
         $("#applyBtn").css("display", "block");
@@ -37,6 +50,4 @@ $("document").ready(function () {
 
      $('#repaySelect').change(() => {
      });
-     
-     
 })
