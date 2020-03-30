b9da4a1c377735e844c545c686c2b93272a18f48
unknown <L00136753@student.lyit.ie>
2020-03-30 01:54:37 +0100

    statement update

--- src/js/statements.js
+++ src/js/statements.js
@@ -1,12 +1,15 @@
+var year, from_month, to_month;
 $('document').ready(() => {
 
-  const customer = sessionStorage.getItem('CustomerID');
-  const account = sessionStorage.getItem('AccountID');
+  const customer_id = sessionStorage.getItem('CustomerID');
+  const account_id = sessionStorage.getItem('AccountID');
 
   addBackButton();
   $.getJSON(`../php/getAccountsTransactions.php?customerID=${customer_id}`, (data) => {
+    populateSelectYears(data, account_id)
     accountTransactions = getAccountTransactions(data, account_id);
     sortAccountTransactions(accountTransactions);
+    handleSelectChanges();
     handleSubmit(accountTransactions);
   });
 
@@ -69,7 +72,64 @@ sortAccountTransactions = (accountTransactions) => {
   }
 }
 
+handleSelectChanges = () => {
+  $('#year-select').change(() => {
+    $('#from-month-select').attr('disabled', false);
+    year = $('#year-select').val();
+  });
+
+  $('#from-month-select').change(() => {
+    from_month = $('#from-month-select').val();
+    if (from_month != 0) {
+      $('#to-month-select-container').attr("style", "display: block");
+      appendToSelect(from_month);
+    }
+    else {
+      $('#to-month-select-container').attr("style", "display: none");
+    }
+  });
+
+  $('#to-month-select').change(() => {
+    to_month = $('#to-month-select').val();
+    console.log(to_month);
+  });
+
+}
+
+handleSubmit = (accountTransactions) => {
+  $('#submitBtn').on('click', () => {
+    console.log("year: ", year, "from: ", from_month, "to", to_month);
+    let data = filterStatementData(accountTransactions);
+    generateStatement(data);
+  });
+}
+
+
+appendToSelect = (month) => {
+  console.log(parseInt(month));
+  $('#to-month-select').empty();
+  let values = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  for (i = parseInt(month) - 1; i < values.length; i++) {
+    console.log(values[i]);
+    $('#to-month-select').append(`<option value=${values[i]}>${months[i]}</option>`);
+  }
+}
+
+
+populateSelectYears = (data, account_id) => {
+  let years = [];
+  for (i in data.accountTransactions) {
+    if (data.accountTransactions[i].AccountID === account_id) {
+      years.push(data.accountTransactions[i].TransDate.substr(0, 4));
+    }
+  }
+  years = _.uniq(years).reverse();
+  for (i in years) {
+    $('#year-select').append(`<option value=${years[i]}>${years[i]}</option`);
+  }
+}
+
 generateStatement = (data) => {
   let statement = new jsPDF();
-
 }
