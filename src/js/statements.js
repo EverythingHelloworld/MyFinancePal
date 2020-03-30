54f6c91c10b7c5896d3a586046bbe64f92479f4b
unknown <L00136753@student.lyit.ie>
2020-03-30 18:09:55 +0100

    satements added

--- src/js/statements.js
+++ src/js/statements.js
@@ -1,4 +1,4 @@
-var year, from_month, to_month;
+var year = 0, from_month = 0, to_month = 0;
 $('document').ready(() => {
 
   const customer_id = sessionStorage.getItem('CustomerID');
@@ -9,6 +9,7 @@ $('document').ready(() => {
     populateSelectYears(data, account_id)
     accountTransactions = getAccountTransactions(data, account_id);
     sortAccountTransactions(accountTransactions);
+    console.log(accountTransactions);
     handleSelectChanges();
     handleSubmit(accountTransactions);
   });
@@ -76,13 +77,28 @@ handleSelectChanges = () => {
   $('#year-select').change(() => {
     $('#from-month-select').attr('disabled', false);
     year = $('#year-select').val();
+    console.log("year ", year, "from ", from_month, "to ", to_month);
+
+    if (year == 0) {
+      $('#from-month-select-container').attr("style", "display: none");
+      $('#to-month-select-container').attr("style", "display: none");
+      $('#from-month-select').val('0');
+      $('#to-month-select').val('0');
+      from_month = 0;
+      to_month = 0;
+    }
+    else {
+      $('#from-month-select-container').attr("style", "display: block");
+    }
   });
+
 
   $('#from-month-select').change(() => {
     from_month = $('#from-month-select').val();
     if (from_month != 0) {
       $('#to-month-select-container').attr("style", "display: block");
       appendToSelect(from_month);
+      console.log("year ", year, "from ", from_month, "to ", to_month);
     }
     else {
       $('#to-month-select-container').attr("style", "display: none");
@@ -91,27 +107,27 @@ handleSelectChanges = () => {
 
   $('#to-month-select').change(() => {
     to_month = $('#to-month-select').val();
-    console.log(to_month);
+    console.log("year ", year, "from ", from_month, "to ", to_month);
   });
+
+
 
 }
 
 handleSubmit = (accountTransactions) => {
   $('#submitBtn').on('click', () => {
     console.log("year: ", year, "from: ", from_month, "to", to_month);
-    let data = filterStatementData(accountTransactions);
+    let data = filterTransactionData(accountTransactions, year, from_month, to_month);
     generateStatement(data);
   });
 }
 
 
 appendToSelect = (month) => {
-  console.log(parseInt(month));
   $('#to-month-select').empty();
   let values = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   for (i = parseInt(month) - 1; i < values.length; i++) {
-    console.log(values[i]);
     $('#to-month-select').append(`<option value=${values[i]}>${months[i]}</option>`);
   }
 }
@@ -131,5 +147,58 @@ populateSelectYears = (data, account_id) => {
 }
 
 generateStatement = (data) => {
-  let statement = new jsPDF();
+  if (data[0].Transactions.length > 0) {
+    let statement = new jsPDF();
+    statement.setFontSize(14);
+    statement.text(10, 10, "Your MyFinancePal Bank Account Statement")
+    statement.text(10, 20, "Account: ")
+    statement.text(10, 30, "Transactions From: All-Time")
+    statement.setFontSize(14);
+    for (i in data) {
+      statement.text(30, 20, data[i].IBAN.toString());
+      statement.setFontSize(10);
+      statement.text(10, 40, "Date");
+      statement.text(50, 40, "Description");
+      statement.text(90, 40, "Type");
+      statement.text(130, 40, "Amount");
+      statement.text(170, 40, "Closing Balance");
+    }
+    for (j in data[i].Transactions) {
+      let ClosingBalance = parseFloat(data[i].Transactions[j].ClosingBalance).toFixed(2).toString();
+      statement.text(10, (parseFloat(j * 10) + 50), formatDate(data[i].Transactions[j].Date));
+      statement.text(50, (parseFloat(j * 10) + 50), data[i].Transactions[j].Description);
+      statement.text(90, (parseFloat(j * 10) + 50), data[i].Transactions[j].Type);
+      statement.text(130, (parseFloat(j * 10) + 50), data[i].Transactions[j].Amount);
+      statement.text(170, (parseFloat(j * 10) + 50), ClosingBalance);
+    }
+    statement.save('statement.pdf');
+  }
+}
+
+
+filterTransactionData = (accountTransactions, from, to) => {
+  dataToReturn = [];
+  for (i in accountTransactions) {
+    dataToReturn.push({
+      "IBAN": accountTransactions[i].IBAN,
+      "CurrentBalance": accountTransactions[i].CurrentBalance,
+      "Transactions": []
+    })
+  }
+
+  for (i in dataToReturn) {
+    for (j in accountTransactions[i].Transactions) {
+      if (accountTransactions[i].Transactions[j].Date.substr(0, 4) === year) {
+        dataToReturn[i].Transactions.push({
+          "Date": accountTransactions[i].Transactions[j].Date,
+          "Description": accountTransactions[i].Transactions[j].Description,
+          "Type": accountTransactions[i].Transactions[j].Type,
+          "Amount": accountTransactions[i].Transactions[j].Amount,
+          "ClosingBalance": accountTransactions[i].Transactions[j].ClosingBalance
+        })
+      }
+    }
+  }
+  console.log(dataToReturn[0].Transactions.length);
+  return dataToReturn;
 }
