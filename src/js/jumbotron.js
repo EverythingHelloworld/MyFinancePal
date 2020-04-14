5c3f0817f268e0edaef16d362d22e0af41e8cee1
EverythingHelloworld <l00088262@student.lyit.ie>
2020-04-15 00:04:50 +0100

    jumbotron styling

--- src/js/jumbotron.js
+++ src/js/jumbotron.js
@@ -1,4 +1,13 @@
 $("document").ready(function () {
-        $("#jumbotron").html("<div class='jumbotron jumbotron-fluid' style='margin:0;'><div class='container'><h1 class='display-4' style='text-align:center;'>" +
-                "My Finance Pal</h1></div></div>");
+        // $("#jumbotron").html("<div class='jumbotron jumbotron-fluid' style='margin:0;'><div class='container'><h1 class='display-4' style='text-align:center;'>" +
+        //         "My Finance Pal</h1></div></div>");
+
+        $("#jumbotron").html(`
+                <div class="jumbotron jumbotron-fluid">
+                        <div class="container">
+                                <center><h1 class="display-3">My Finance Pal</h1></center>
+                                <center><p class="lead">Your perfect banking partner because life's complicated enough.</p></center>
+                        </div>
+                </div>      
+        `);
 })
